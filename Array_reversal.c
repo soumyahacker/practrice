745c1f4abccae6794b213f4469e8c153b7ffbf38
soumyahacker <pg2196577@gmail.com>
2024-06-02 22:07:57 +0530

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   Array_reversal.c 	new file:   Array_reversal.exe

--- Array_reversal.c
+++ Array_reversal.c
@@ -1,23 +1,28 @@
 #include <stdio.h>
 
-void swap(int *a, int *b) {
+void swap(int *a, int *b)
+{
     int temp = *a;
     *a = *b;
     *b = temp;
 }
 
-void reversal(int arr[], int size) {
-    for (int i = 0; i < size / 2; i++) {
+void reversal(int arr[], int size)
+{
+    for (int i = 0; i < size / 2; i++)
+    {
         swap(&arr[i], &arr[size - i - 1]);
     }
 }
 
-int main() {
+int main()
+{
     int arr[] = {1, 3, 4, 0, 5, 4, 3, 3};
     int size = sizeof(arr) / sizeof(arr[0]);
 
     printf("Original array: ");
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++)
+    {
         printf("%d ", arr[i]);
     }
     printf("\n");
@@ -25,7 +30,8 @@ int main() {
     reversal(arr, size);
 
     printf("Reversed array: ");
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++)
+    {
         printf("%d ", arr[i]);
     }
     printf("\n");
